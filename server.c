d8d58df79b7f358cd3e64f10f771d779b77cb8ae
Anish-Udupa <udupa.anish@gmail.com>
2022-03-21 22:27:00 +0530

    Basic interface laid

--- server.c
+++ server.c
@@ -5,41 +5,69 @@
 #include <string.h>
 #include <unistd.h>
 
-void remove_newline(char *str){
-	for(int i=0; str[i] != '\0'; i++){
-		if(str[i] == '\n'){
-			str[i] = '\0';
-			break;
-		}
-	}
-	printf("rmv newline: %s", str);
-}
+#define INP_BUF_SIZE 100
 
 void close_socket(int socket_desc){
 	close(socket_desc);
 }
 
+void strip_rn(char *str, int n){
+    // n is inclusive of \r\n
+    str[n-2] = '\0';
+}
+
 int authenticate(int new_socket){
     write(new_socket, "Enter your username: ", 21);
-    char username[100];
-    read(new_socket, username, 100);
-    remove_newline(username);
-    
-    write(new_socket, "Enter your password: ", 21);
-    char password[100];
-    read(new_socket, password, 100);
-    //remove_newline(password);
-    
-    printf("123 %s %s", username, password); 
-    
-    if(strcmp(username, "admin\n") == 0 && strcmp(password, "admin\n") == 0){
-        write(new_socket, "Authentication successful.\n", 30);
-        printf("Success");
-        return 1;
-    }
-    else{
-        write(new_socket, "Authentication failed.\n", 25);
+    char username[10];
+    int size_usr = read(new_socket, username, 10);
+    if(size_usr == -1){
+        printf("Error while receiving the username.\n");
         return 0;
+    }
+    strip_rn(username, size_usr);
+    // write(new_socket, "Enter your password: ", 21);
+    // char password[100];
+    // read(new_socket, password, 5);
+    
+    // if(strcmp(username, "admin\n") == 0 && strcmp(password, "admin\n") == 0){
+    //     write(new_socket, "Authentication successful.\n", 30);
+    //     printf("Success");
+    //     return 1;
+    // }
+    // else{
+    //     write(new_socket, "Authentication failed.\n", 25);
+    //     return 0;
+    // }
+
+    write(new_socket, "Authentication Successful!\n", 28);
+    return 1;
+}
+
+void interact(int new_socket){
+    write(new_socket, "Enter help for list of all commands.\n", 38);
+    int no_exit = 1;
+    char input[INP_BUF_SIZE];
+    int read_size;
+    char *op;
+    while(no_exit){
+        write(new_socket, ">> ", 4);
+        read_size = read(new_socket, input, 100);
+        strip_rn(input, read_size);
+        op = strtok(input, " ");
+
+        // Define operations
+        if(strcmp(op, "help") == 0){
+            // Help command
+            write(new_socket, "Help command\n", 14);
+        }
+        else if(strcmp(op, "quit") == 0){
+            // Quit command
+            write(new_socket, "Quitting...\n", 13);
+            no_exit = 0;
+        }
+        else{
+            write(new_socket, "Invalid command\n", 17);
+        }
     }
 }
 
@@ -73,12 +101,13 @@ int main(){
     c = sizeof(struct sockaddr_in);
     while(new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)){
         printf("Connection accepted.\n");
-        // char *message = "Hello, client!\n";
-        // send(new_socket, message, strlen(message), 0);
         int val = authenticate(new_socket);
-        if (val == 1){
-            // Auth successful
+        if (val != 1){
+            // Auth failed
+            close(new_socket);
+            continue;
         }
+        interact(new_socket);
         close(new_socket);
     }
 
